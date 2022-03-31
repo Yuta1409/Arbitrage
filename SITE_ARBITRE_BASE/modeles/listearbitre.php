e95957dfd21ce2341eed2185993bc56c3f30a1f9
ROGER <yroger2003@gmail.com>
2022-03-31 10:29:58 +0200

    avant config fonction

---
+++ SITE_ARBITRE_BASE/modeles/listearbitre.php
@@ -0,0 +1,44 @@
+﻿<h2>ARBITRE ...</h2>
+
+<?php
+include "bd_connexion.php";
+
+function getArbitres(){
+    $resultat = array();
+
+    try{
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select num_equipe, nom_equipe, num_club, num_championnat");
+        $req->execute();
+
+        $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+
+    }catch (PDOException $e) {
+        print "Erreur !:" . $e->getMessage();
+    }
+    return $resultat;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- SITE_ARBITRE_BASE/pages/gestionequipe.php
+++ SITE_ARBITRE_BASE/pages/gestionequipe.php
@@ -1,73 +1,99 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Gestion des équipes</title>
-    <link href="style.css" rel="stylesheet">
-  </head>
-  <body>
-  </body>
-    <?php
-        include "bd_connexion.php";
-        if(isset($_GET['btnAjouter'])){
-          $connex = connexionPDO();
-          $req="INSERT INTO equipe(nom_equipe, num_club, num_championnat) VALUES(:param1, :param2 ,:param3)";
-          $prep = $connex->prepare($req);
-          $prep->bindValue('param1', $_GET['nom_equipe']);
-          $prep->bindValue('param2', $_POST['num_club']);
-          $prep->bindValue('param3', $_POST['num_championnat']);
-          $prep->execute();
 
-            $afficher = "SELECT * FROM dalton";
-            $req2 = $connex->prepare($afficher);
-            $req2->execute();
-            echo "<h1> Equipe.</h1>";
-            while($ligne = $req2->fetch(PDO::FETCH_OBJ)) {
-                echo "$ligne->num_equipe $ligne->nom_equipe $ligne->num_club $ligne->num_championnat<br/>";
+<head>
+  <meta charset="utf-8">
+  <title>Gestion des équipes</title>
+  <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+</body>
+
+<?php
+include "bd_connexion.php";
+if (isset($_GET['btnAjouter'])) {
+  $connex = connexionPDO();
+  $req = "INSERT INTO equipe(nom_equipe, num_club, num_championnat) VALUES(:param1, :param2 ,:param3)";
+  $prep = $connex->prepare($req);
+  $prep->bindValue('param1', $_GET['nom_equipe']);
+  $prep->bindValue('param2', $_POST['num_club']);
+  $prep->bindValue('param3', $_POST['num_championnat']);
+  $prep->execute();
+
+  $afficher = "SELECT * FROM dalton";
+  $req2 = $connex->prepare($afficher);
+  $req2->execute();
+  echo "<h1> Equipe.</h1>";
+  while ($ligne = $req2->fetch(PDO::FETCH_OBJ)) {
+    echo "$ligne->num_equipe $ligne->nom_equipe $ligne->num_club $ligne->num_championnat<br/>";
+  }
+} else {
+}
+?>
+<form method="GET" action="">
+  <fieldset>
+    <legend>Gestion de la competition :</legend>
+    <p>
+      <br>
+      <label for="taille">nom_equipe :</label>
+      <input type="text" name="nom_equipe" size="20" />
+      <br><br>
+      <label for="taille">nom_club :</label>
+      <select name="num_club" class="retour">
+
+        <?php
+        $connex = connexionPDO();
+        $req = $connex->prepare("SELECT * FROM club");
+        $req->execute();
+        while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
+          echo "<option value= '$ligne->num_club'>$ligne->nom_club</option>";
         }
-    }
-          else{
-          }
-          ?>
-        <form method="GET"action="">
-        <fieldset>
-          <legend>Gestion de la competition :</legend>
-          <p>
-            <br>
-            <label for="taille">nom_equipe :</label>
-            <input type="text" name="nom_equipe" size="20" />
-            <br><br>
-            <label for="taille">nom_club :</label>
-            <select name="num_club" class = "retour" >
+        ?>
+      </select>
 
-                <?php
-                $connex = connexionPDO();
-                $req = $connex->prepare("SELECT * FROM club");
-                $req->execute();
-                while($ligne = $req->fetch(PDO::FETCH_OBJ)){
-                    echo"<option value= '$ligne->num_club'>$ligne->nom_club</option>";
-                }
-                ?>
-            </select>
+      <br><br>
+      <label for="taille">num_championnat :</label>
+      <select name="num_championnat" class="retour">
 
-            <br><br>
-            <label for="taille">num_championnat :</label>
-            <select name="num_championnat" class = "retour" >
+        <?php
+        $connex = connexionPDO();
+        $req = $connex->prepare("SELECT num_championnat, nom_championnat FROM championnat");
+        $req->execute();
+        while ($ligne = $req->fetch(PDO::FETCH_OBJ)) {
+          echo "<option value= '$ligne->num_championnat'>$ligne->nom_championnat</option>";
+        }
+        ?>
+      </select>
+    </p>
+    <p>
+      <button type="submit" value="ajouter" name="btnAjouter">Ajouter</button>
+    </p>
+    
+    <table id="ta">
+      
+      <tr>
+        <th>Gestion des Equipes</th>
+      </tr>
+      <tr>
+        <td>n° equipe</td>
+        <td>nom equipe</td>
+        <td>nom club</td>
+        <td>nom championnat</td>
+      </tr>
+      <tr>
+        <td><input class="txt" type="texte" name="txtNumeroEquipe"></td>
+        <td><input class="txt" type="texte" name="txtNomEquipe"></td>
+        <td><input class="txt" type="texte" name="txtNomClub"></td>
+        <td><input class="txt" type="texte" name="txtNomChamp"></td>
+        <td><input class="txt" type="submit" name="mod" value="modifier"></td>
+        <td><input class="txt" type="submit" name="supp" value="supprimer"></td>
+      </tr>
+    </table>
 
-                <?php
-                $connex = connexionPDO();
-                $req = $connex->prepare("SELECT num_championnat, nom_championnat FROM championnat");
-                $req->execute();
-                while($ligne = $req->fetch(PDO::FETCH_OBJ)){
-                    echo"<option value= '$ligne->num_championnat'>$ligne->nom_championnat</option>";
-                }
-                ?>
-            </select>
-          </p>
-          <p>
-            <button type="submit" value="ajouter" name="btnAjouter">Ajouter</button>
-          </p>
-        </fieldset>
-            </select>
-        </form>
+
+  </fieldset>
+  </select>
+</form>
+
 </html>
